60b37b9338119c2a6bb253ab5adc60d05fbfdcc9
ajitrai1507 <ajitrai0715@gmail.com>
2025-01-27 22:26:11 +0530

    basics of typescript

---
+++ 02/first.js
@@ -0,0 +1,13 @@
+"use strict";
+// let a:number = 10;
+// a = "Ajit Rai";
+// let b:String = "Ajit Rai"
+// b= "Rai ji"
+// b= 10;
+// console.log(b)
+// let isLogin:Boolean = false;
+// isLogin = true
+// console.log(isLogin)
+let a = "Ajit Rai";
+a = Math.random();
+console.log(a);
---
+++ 02/first.ts
@@ -0,0 +1,20 @@
+// let a:number = 10;
+
+// a = "Ajit Rai";
+
+// let b:String = "Ajit Rai"
+// b= "Rai ji"
+
+// b= 10;
+// console.log(b)
+
+// let isLogin:Boolean = false;
+
+// isLogin = true
+// console.log(isLogin)
+
+let a:any = "Ajit Rai";
+
+a= Math.random()
+
+console.log(a)
